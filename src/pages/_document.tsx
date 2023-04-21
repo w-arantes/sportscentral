aa153ca803da8d8fbc12f9af3ebd6c7f5843fda9
Wellington Arantes <w.aranteslfc@gmail.com>
2023-04-21 19:34:27 -0300

    chore: updating document configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class AppDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,700;0,900;1,200&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" type="image" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
