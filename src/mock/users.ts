d96f943fad89e0594ff5dc141cf79d0562629f54
Wellington Arantes <w.aranteslfc@gmail.com>
2023-04-23 15:44:28 -0300

    feat: manage users page initial layout with mockdata

---
+++ src/mock/users.ts
@@ -0,0 +1,71 @@
+export const users = [
+  {
+    id: '800532af-4b1a-4f1f-8feb-16d2f9c1b8f4',
+    name: 'Wellington',
+    surname: 'Arantes',
+    email: 'w.arantes@sportscentral.com',
+    isAdmin: true
+  },
+  {
+    id: 'c6d677a6-76a6-4b1b-a74c-57dd7b2600cc',
+    name: 'Admin',
+    surname: 'SportsCentral',
+    email: 'admin@sportscentral.com'
+  },
+  {
+    id: 'c6d677a6-76a6-4b1b-a74c-57dd7b2600c3',
+    name: 'Jürgen',
+    surname: 'Klopp',
+    email: 'j.klopp@example.com',
+    isAdmin: true
+  },
+  {
+    id: '9c9a04a9-650e-4a37-8c16-7cf34de3d3a1',
+    name: 'Mario',
+    surname: 'Götze',
+    email: 'm.goetze@example.com',
+    isAdmin: false
+  },
+  {
+    id: 'a5500566-cd21-4b8f-946a-09b58f42531a',
+    name: 'Steven',
+    surname: 'Gerrard',
+    email: 's.gerrard@example.com',
+    isAdmin: false
+  },
+  {
+    id: '191b6f15-6f27-4a88-9be7-aae9b6db2b32',
+    name: 'Emre',
+    surname: 'Can',
+    email: 'e.can@example.com',
+    isAdmin: false
+  },
+  {
+    id: 'c5e5f5df-2316-42c6-95eb-c6011b6f547e',
+    name: 'Timo',
+    surname: 'Klock',
+    email: 't.klock@example.com',
+    isAdmin: false
+  },
+  {
+    id: '7d93a052-9096-48c6-8f6b-61ec62e3f7a3',
+    name: 'Timo',
+    surname: 'Werner',
+    email: 't.werner@example.com',
+    isAdmin: false
+  },
+  {
+    id: 'd5f01d5b-9c0d-42af-bfe5-903c47e9881c',
+    name: 'Ayrton',
+    surname: 'Senna',
+    email: 'a.senna@example.com',
+    isAdmin: false
+  },
+  {
+    id: '49a9d8c4-7a1a-4f69-9e8c-163f7e2f9a71',
+    name: 'Augusto',
+    surname: 'Farfus',
+    email: 'a.farfus@example.com',
+    isAdmin: false
+  }
+];
---
+++ src/pages/dashboard/users/index.tsx
@@ -0,0 +1,105 @@
+import {
+  Button,
+  IconButton,
+  Flex,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  Tooltip
+} from '@chakra-ui/react';
+import { TrashSimple, PencilSimple } from '@phosphor-icons/react';
+
+import { PageLayout } from '@/layout';
+import { users } from '@/mock';
+
+export default function ManageUsers() {
+  const handleRegisterNew = () => {
+    console.log('handleRegisterNew');
+  };
+
+  const handleEditUser = () => {
+    console.log('handleEditUser');
+  };
+
+  const handleDelete = () => {
+    console.log('handleDelete');
+  };
+
+  return (
+    <PageLayout title="Manage Users | SportsCentral" mt="1rem">
+      <Flex direction="column" align="center" justify="center">
+        <Text fontSize="title" fontWeight="bold" color="white">
+          Manage Users
+        </Text>
+      </Flex>
+
+      <Flex
+        direction="column"
+        w="100%"
+        h="100%"
+        bg="gray.medium"
+        mt="2rem"
+        p="4rem"
+      >
+        <Flex direction="row" align="center" justify="space-between">
+          <Text color="white">All Users: {users?.length}</Text>
+          <Button onClick={handleRegisterNew}>NEW USER</Button>
+        </Flex>
+
+        <TableContainer mt="2rem">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th color="gray.light">Id</Th>
+                <Th color="gray.light">Name</Th>
+                <Th color="gray.light">Surname</Th>
+                <Th color="gray.light">Admin Features</Th>
+                <Th color="gray.light">E-mail</Th>
+                <Th color="gray.light">Actions</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users &&
+                users.map((user) => {
+                  return (
+                    <Tr key={user.id}>
+                      <Td>{user?.id}</Td>
+                      <Td>{user?.name}</Td>
+                      <Td>{user?.surname}</Td>
+                      <Td>{user?.isAdmin ? 'Yes' : 'No'}</Td>
+                      <Td>{user?.email}</Td>
+                      <Td>
+                        <Tooltip label="Edit User" openDelay={500}>
+                          <IconButton
+                            variant="unstyled"
+                            size="md"
+                            aria-label="Edit User"
+                            icon={<PencilSimple size={20} color="#00B37E" />}
+                            onClick={handleEditUser}
+                          />
+                        </Tooltip>
+                        <Tooltip label="Delete User" openDelay={500}>
+                          <IconButton
+                            variant="unstyled"
+                            size="md"
+                            aria-label="Delete User"
+                            icon={<TrashSimple size={20} color="#F75A68" />}
+                            onClick={handleDelete}
+                          />
+                        </Tooltip>
+                      </Td>
+                    </Tr>
+                  );
+                })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
+    </PageLayout>
+  );
+}
