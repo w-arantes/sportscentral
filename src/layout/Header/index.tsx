44ea17a7cc240551433a71be2290ce0d5d1e0d68
Wellington Arantes <w.aranteslfc@gmail.com>
2023-04-25 23:07:14 -0300

    chore: updating header navigation

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -25,7 +25,7 @@ export function Header() {
       as="header"
       bg="gray.medium"
       justify="space-between"
-      alignItems="center"
+      align="center"
       w="100%"
       h="80px"
       px="146px"
@@ -40,63 +40,69 @@ export function Header() {
         w="200px"
         h="60px"
       />
-      <Flex direction="row" align="flex-end" justify="flex-end">
-        <HStack spacing="1rem">
-          {isAuthenticated ? (
-            <>
-              <Tooltip label="View and manage your profile" openDelay={500}>
-                <Button
-                  variant="header"
-                  onClick={() => push('/dashboard/profile')}
-                >
-                  PROFILE
-                </Button>
-              </Tooltip>
-              {credentials?.isAdmin && (
-                <Menu>
-                  <MenuButton>MANAGE</MenuButton>
-                  <MenuList>
-                    <Tooltip label="Manage Users" openDelay={1000}>
-                      <MenuItem as="a" href="/dashboard/admin/users">
-                        USERS
-                      </MenuItem>
-                    </Tooltip>
-                    <Tooltip label="Manage Events" openDelay={1000}>
-                      <MenuItem as="a" href="/dashboard/admin/events">
-                        EVENTS
-                      </MenuItem>
-                    </Tooltip>
-                    <Tooltip label="Manage Categories" openDelay={1000}>
-                      <MenuItem as="a" href="/dashboard/admin/categories">
-                        CATEGORIES
-                      </MenuItem>
-                    </Tooltip>
-                  </MenuList>
-                </Menu>
-              )}
-              <Tooltip label="Sign-out" openDelay={500}>
-                <IconButton
-                  variant="unstyled"
-                  size="md"
-                  aria-label="Sign-Out"
-                  icon={<SignOut size={20} color="#F75A68" />}
-                  onClick={signOut}
-                />
-              </Tooltip>
-              <Avatar
-                name={`${credentials?.name + ' ' + credentials?.surname}`}
-                bg="brand.light"
+      <HStack spacing="2rem" align="center" justify="center">
+        {isAuthenticated ? (
+          <>
+            <Menu>
+              <MenuButton>PROFILE</MenuButton>
+              <MenuList>
+                <Tooltip label="Manage Users" openDelay={1000}>
+                  <MenuItem as="a" href="/dashboard/profile">
+                    SETTINGS
+                  </MenuItem>
+                </Tooltip>
+                <Tooltip label="Manage Events" openDelay={1000}>
+                  <MenuItem as="a" href="/dashboard/following">
+                    SUBSCRIPTIONS
+                  </MenuItem>
+                </Tooltip>
+              </MenuList>
+            </Menu>
+
+            {credentials?.isAdmin && (
+              <Menu>
+                <MenuButton>MANAGE</MenuButton>
+                <MenuList>
+                  <Tooltip label="Manage Users" openDelay={1000}>
+                    <MenuItem as="a" href="/dashboard/admin/users">
+                      USERS
+                    </MenuItem>
+                  </Tooltip>
+                  <Tooltip label="Manage Events" openDelay={1000}>
+                    <MenuItem as="a" href="/dashboard/admin/events">
+                      EVENTS
+                    </MenuItem>
+                  </Tooltip>
+                  <Tooltip label="Manage Categories" openDelay={1000}>
+                    <MenuItem as="a" href="/dashboard/admin/categories">
+                      CATEGORIES
+                    </MenuItem>
+                  </Tooltip>
+                </MenuList>
+              </Menu>
+            )}
+            <Tooltip label="Sign-out" openDelay={500}>
+              <IconButton
+                variant="unstyled"
+                size="md"
+                aria-label="Sign-Out"
+                icon={<SignOut size={20} color="#F75A68" />}
+                onClick={signOut}
               />
-            </>
-          ) : (
-            <Tooltip label="Log-in into SportsCentral" openDelay={500}>
-              <Button variant="header" onClick={() => push('/login')}>
-                LOGIN
-              </Button>
             </Tooltip>
-          )}
-        </HStack>
-      </Flex>
+            <Avatar
+              name={`${credentials?.name + ' ' + credentials?.surname}`}
+              bg="brand.light"
+            />
+          </>
+        ) : (
+          <Tooltip label="Log-in into SportsCentral" openDelay={500}>
+            <Button variant="header" onClick={() => push('/login')}>
+              LOGIN
+            </Button>
+          </Tooltip>
+        )}
+      </HStack>
     </Flex>
   );
 }
