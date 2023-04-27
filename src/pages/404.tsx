56c423bf11c88c31e4210b785cd301b1ce24f848
Wellington Arantes <w.aranteslfc@gmail.com>
2023-04-26 22:17:59 -0300

    feat: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,36 @@
-import { Center, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Center, Flex, Text, Link } from '@chakra-ui/react';
+
+import { PageLayout } from '@/layout';
 
 export default function NotFound() {
   return (
-    <Center>
-      <Text>This page could not be found.</Text>
-    </Center>
+    <PageLayout title="Not Found">
+      <Center width="100%" height="500px" mt="2rem" bg="gray.medium">
+        <Flex direction="column" align="center" justify="center">
+          <Text fontSize="hero" fontWeight="bold" color="white">
+            Page not found!
+          </Text>
+          <Text fontSize="h1">
+            Sorry, but the page you were looking for could not be found.
+          </Text>
+
+          <Link
+            as={NextLink}
+            href="/"
+            mt="2rem"
+            _hover={{
+              textDecoration: 'none',
+              color: 'brand.primary',
+              transition: 'color 0.5s ease'
+            }}
+          >
+            <Text fontSize="h1" fontWeight="bold">
+              Return to home page
+            </Text>
+          </Link>
+        </Flex>
+      </Center>
+    </PageLayout>
   );
 }
